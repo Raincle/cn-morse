9695c31cf9ded58618868ae69da8d797dd2d8777
Yawei <yawei@i-hong.com>
2022-07-16 18:09:57 +0800

    error check

--- src/decode.ts
+++ src/decode.ts
@@ -3,8 +3,13 @@ import { CODE_MAP } from './codeMap';
 
 export function decode(morse: string): string {
     let morseArr = morse.split(' ');
-    return morseArr.map((code) => {
-        return CODE_MAP[code]
+    return morseArr.map((code: string, index) => {
+        const ch = CODE_MAP[code]
+        if (ch) {
+            return ch
+        } else {
+            throw new Error(`解密失败，请检查第${index + 1}个密码: ${code}`);
+        }
     })
     .join('')
 }
--- src/encode.ts
+++ src/encode.ts
@@ -3,9 +3,13 @@ import { CH_MAP } from './chMap';
 
 export function encode(chStr: string): string {
     let chArr = chStr.split('')
-    return chArr.map((ch: string) => {
+    return chArr.map((ch: string, index) => {
         const code = CH_MAP[ch]
-        return code
+        if (code) {
+            return code
+        } else {
+            throw new Error(`加密失败，请检查第${index + 1}个中文: ${ch}`);
+        }
     })
     .join(' ')
 }
